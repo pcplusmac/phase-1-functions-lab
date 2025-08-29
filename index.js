19ffac721f8eef600eea3cacba7d99386d0a0d13
el-sam <samurai.h@yahoo.com>
2025-08-29 22:16:38 +0800

    3rd function completed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Code your solution in this file!
 const hq = 42;
 let pickup;
+let startBlock;
+let endBlock;
 
 function distanceFromHqInBlocks(pickup) {
     let distanceInBlock;
@@ -23,3 +25,19 @@ function distanceFromHqInFeet(pickup) {
     return (distanceInFeet);
 }
 
+function distanceTravelledInFeet(startBlock,endBlock) {
+    let blocksTravel;
+    let distanceTravel;
+    
+    if (startBlock > endBlock) {
+        blocksTravel = startBlock - endBlock;
+        distanceTravel = blocksTravel * 264;
+        return distanceTravel;
+    } else if (startBlock < endBlock) {
+        blocksTravel =  endBlock - startBlock ;
+        distanceTravel = blocksTravel * 264;
+        return distanceTravel;
+    } else {
+        return "lazy!"
+    }
+}
