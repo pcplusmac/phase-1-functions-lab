16c19c0c49cf4d6dad86cc6fbbfb06d4146dcc60
el-sam <samurai.h@yahoo.com>
2025-08-29 21:56:31 +0800

    completed function 1

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+const hq = 42;
+let pickup;
+
+function distanceFromHqInBlocks(pickup) {
+    let distanceInBlock;
+    
+    if (pickup > 42){
+        distanceInBlock = pickup - 42;
+        return (distanceInBlock);
+    }  else if (pickup < 42) {
+        distanceInBlock = 42 - pickup;
+        return (distanceInBlock);
+
+    } else {
+        return "you are not moving. "
+    }
+}
+
